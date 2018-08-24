84c509ed92ad529404a9fa32dee2d2a4f7caffd5
Charmi Srivastava <chasriva0@publicisgroupe.net>
2018-08-24 15:55:10 +0530

    testing bug

--- vars/carnivalpipeline.groovy
+++ vars/carnivalpipeline.groovy
@@ -79,7 +79,7 @@ def call(body)
         try{
           //echo "${MAVEN_VERSION}"
           def TASKS = "clean install"
-          maven.mavenbuild("${WORKSPACE}\addressbook_main" , "${TASKS}")
+          maven.mavenbuild("${WORKSPACE}" , "${TASKS}")
           }
           catch (Exception error){
           wrap([$class: 'AnsiColorBuildWrapper']) {
